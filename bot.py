eec7e71adfe89992c6a97d1c43f2ee2b5560befc
adenizgelir <adenizgelir@turtapps.com>
2016-04-02 09:45:23 +0300

    bot.py
    bot's file

---
+++ bot.py
@@ -0,0 +1,20 @@
+import webbrowser
+import time
+import os
+
+print "BY TURTAPPS"
+print "Subscribe to 'open coder' in youtube"
+
+url = raw_input("URL>")
+refresh = 10
+brow = raw_input("browser>")
+views = int(raw_input("view>"))
+
+
+def bot():
+	os.system(" killall -9 " + brow)
+	webbrowser.open(url)
+	time.sleep(refresh)
+
+for i in range(views):
+	bot()
